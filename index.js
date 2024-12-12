424fef5b0c5f0b29be07cabff04656e91622e5e3
Justin <justin-urbanek@bluewin.ch>
2024-12-12 09:17:08 +0100

    formatter test

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const myUnusedVariable = 42;
 const someVariable = 10;
 
-if (someVariable >= myUnusedVariable) {
-  console.log("intentional linting error.");
+if (
+  someVariable >=
+  myUnusedVariable
+) {
+  console.log(
+    'intentional linting error.',
+  );
 }
